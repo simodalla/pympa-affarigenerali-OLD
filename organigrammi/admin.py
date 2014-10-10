3abb73a305d9a1303e16fe1722dfc16e0c5e7b74
Simone Dalla <simodalla@gmail.com>
2014-10-10 09:45:54 +0200

    add AssessoreAdmin

--- organigrammi/admin.py
+++ organigrammi/admin.py
@@ -32,7 +32,7 @@ class ConsigliereInline(admin.TabularInline):
 @admin.register(Persona)
 class PersonaAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('None', {'fields': ('cognome', 'nome', 'email', 'ente', 'user')}),
+        (None, {'fields': ('cognome', 'nome', 'email', 'ente', 'user')}),
         RANGE_VALIDITA_SECTION)
     list_display = ('cognome', 'nome', 'ente', 'user', 'inizio_validita',
                     'fine_validita')
@@ -42,7 +42,7 @@ class PersonaAdmin(admin.ModelAdmin):
 @admin.register(Ente)
 class EnteAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('None', {'fields': ('titolo', 'ente_padre')}),
+        (None, {'fields': ('titolo', 'ente_padre')}),
         RANGE_VALIDITA_SECTION)
     list_display = ('titolo', 'ente_padre')
 
@@ -50,8 +50,8 @@ class EnteAdmin(admin.ModelAdmin):
 @admin.register(Mandato)
 class MandatoAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('None', {'fields': ('ente', 'boss', 'vice', 'speacker',
-                             'vice_speacker')}),
+        (None, {'fields': ('ente', 'boss', 'vice', 'speacker',
+                           'vice_speacker')}),
         RANGE_VALIDITA_SECTION)
     list_display = ('ente', 'inizio_validita', 'fine_validita', 'boss', 'vice',
                     'speacker')
@@ -62,8 +62,8 @@ class MandatoAdmin(admin.ModelAdmin):
 @admin.register(Consigliere)
 class ConsigliereAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('None', {'fields': ('persona', 'mandato', 'gruppoconsigliare',
-                             'capogruppo',)}),
+        (None, {'fields': ('persona', 'mandato', 'gruppoconsigliare',
+                           'capogruppo',)}),
         RANGE_VALIDITA_SECTION)
     list_filter = ('gruppoconsigliare',)
     list_display = ('persona', 'mandato', 'gruppoconsigliare',
@@ -71,13 +71,24 @@ class ConsigliereAdmin(admin.ModelAdmin):
     search_fields = ('persona__cognome', 'persona__nome',)
 
 
+@admin.register(Assessore)
+class AssessoreAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('persona', 'mandato', 'delega',)}),
+        RANGE_VALIDITA_SECTION)
+    list_filter = ('mandato',)
+    list_display = ('persona', 'mandato', 'delega',
+                    'inizio_validita', 'fine_validita')
+    search_fields = ('persona__cognome', 'persona__nome', 'delega')
+
+
 @admin.register(CommissioneConsigliare)
 class CommissioneConsigliareAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('None', {'fields': ('mandato', 'titolo', 'boss', 'vice',
-                             'componenti')}),
+        (None, {'fields': ('mandato', 'titolo', 'boss', 'vice',
+                           'componenti')}),
     )
-    filter_horizontal = ('componenti',)
+    filter_horizontal = ('componenti', )
     list_display = ('titolo', 'mandato', 'boss', 'vice', 'ld_componenti')
     list_filter = ('mandato',)
     search_fields = ('titolo', 'mandato__ente__titolo',)
@@ -89,9 +100,10 @@ class CommissioneConsigliareAdmin(admin.ModelAdmin):
         links = ['<a href="{}">{}</a>'.format(
             c.get_absolute_url(), c.persona) for c in obj.componenti.all()]
         return '<br />'.join(links)
+
     ld_componenti.short_description = 'Componenti'
     ld_componenti.allow_tags = True
-    
+
     def get_search_fields(self, request):
         search_fields = list(super(
             CommissioneConsigliareAdmin, self).get_search_fields(request))
